c0ce78099321ba11b7cb3f7cd32c5dac48b75f32
Alyssa Kennedy <alyssadrandall@gmail.com>
2018-11-24 03:19:16 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,12 @@
 def unsafe?(speed)
-  if speed > 60 
-
+  speed > 60 
+end
+end
 
 
 
 def not_safe?(speed)
-	if speed > 60 || speed < 40 ? "true" : "false"
+	if speed > 60 || speed < 40 ? true : "false"
 
 	
 
